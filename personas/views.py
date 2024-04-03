ecbf87a28c3081b57846b7c1f3945f237099b6b8
SamuelStinson02 <samuelstinson02@gmail.com>
2024-04-02 20:02:07 -0400

    First Commit

---
+++ personas/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Persona
+from .forms import PersonaForm
+
+
+def lista_personas(request):
+    personas = Persona.objects.all()
+    return render(request, 'personas/lista_personas.html', {'personas': personas})
+
+
+def agregar_persona(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_personas')
+    else:
+        form = PersonaForm()
+    return render(request, 'personas/agregar_persona.html', {'form': form})
